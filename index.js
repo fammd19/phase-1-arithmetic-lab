30b575ea806e4edd6cfed58968a01d053d41c5e5
Fi Mellink <fimellink@gmail.com>
2023-12-07 15:26:46 +1100

    Submit lab exercise

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+//console.log (multiply);
+
+let random = Math.round(Math.random()*100);
+
+//console.log (random);
+
+let num3 = 16;
+let num4 = 4;
+
+let mod = num3/num4;
+
+//console.log(mod);
+
+let max = Math.max(5,8,11,14,17,20);
+
+//console.log(max);
